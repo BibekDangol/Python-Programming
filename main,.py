e6f8f5a88e676904cfeaae1644db8c9f72771de0
Bibek Raj Dangol <bibekdngl@gmail.com>
2024-11-13 10:29:02 +0545

    Week 3-Data Analysis

---
+++ main,.py
@@ -0,0 +1,12 @@
+import math_utils 
+import Mathematics.trigonometry 
+from Mathematics import trigonometry # recon
+import Mathematics
+from Mathematics.trigonometry import sine 
+
+sum = math_utils.add(1, 2)
+diff = math_utils.subtract(2, 1)
+print(sum, diff)
+pi = 3.14
+sin_45 = Mathematics.trigonometry.sine(pi/4)
+print(sin_45)
